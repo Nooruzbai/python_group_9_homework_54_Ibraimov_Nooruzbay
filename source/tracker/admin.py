f39bbf9d5a9df7adedcb14a90b9214ac1f27f0c0
Nooruzbay <nooruzbay@gmail.com>
2022-01-12 23:11:42 +0600

    Migrated the models.py, created the superuser, added some data from the admin side

--- source/tracker/admin.py
+++ source/tracker/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from tracker.models import Task, Status, Type
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'summary', 'status', 'date_created', 'date_updated']
+
+
+admin.site.register(Status)
+admin.site.register(Type)
+admin.site.register(Task, TaskAdmin)
+
