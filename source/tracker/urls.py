41b22233d2690da29210dc6adab287984d44444c
Nooruzbay <nooruzbay@gmail.com>
2022-01-13 12:35:55 +0600

    Created function deteiled view

--- source/tracker/urls.py
+++ source/tracker/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from tracker.views import IndexView
+from tracker.views import IndexView, TaskView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name="index_view")
+    path('', IndexView.as_view(), name='index_view'),
+    path('details/<int:pk>', TaskView.as_view(), name='task_view')
 ]
--- source/tracker/views.py
+++ source/tracker/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import View, TemplateView
 from tracker.models import Task
 
@@ -9,3 +9,13 @@ class IndexView(TemplateView):
     def get(self, request, *args, **kwargs):
         tasks = Task.objects.all()
         return render(request, 'index.html', {'tasks': tasks})
+
+
+class TaskView(TemplateView):
+    template_name = 'task_view.html'
+
+    def get_context_data(self, **kwargs):
+        task = get_object_or_404(Task, pk=kwargs.get('pk'))
+        kwargs['task'] = task
+        return super().get_context_data(**kwargs)
+
