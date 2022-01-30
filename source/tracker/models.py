50359c42bd9c2858daf00fb0e03c0fd55a095597
Nooruzbay <nooruzbay@gmail.com>
2022-01-30 11:49:49 +0600

    Created Project Model and migrated

--- source/tracker/models.py
+++ source/tracker/models.py
@@ -37,6 +37,7 @@ class Task(models.Model):
     type = models.ManyToManyField('tracker.Type', related_name='tasks', verbose_name='Type')
     date_created = models.DateTimeField(auto_now_add=True, verbose_name="Date created")
     date_updated = models.DateTimeField(auto_now=True, verbose_name="Date updated")
+    project = models.ForeignKey('tracker.Project', related_name='tasks', on_delete=models.CASCADE, verbose_name='Projects')
 
     def __str__(self):
         return f'{self.pk}. {self.summary} {self.status} {self.type}'
@@ -47,6 +48,19 @@ class Task(models.Model):
         verbose_name_plural = 'Tasks'
 
 
+class Project(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name="Project Name")
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name="Description")
+    date_started = models.DateTimeField(null=True, blank=True, verbose_name="Date created")
+    date_closed = models.DateTimeField(null=True, blank=True, verbose_name="Date closed")
+
+    def __str__(self):
+        return f'{self.pk}. {self.name} {self.date_started} {self.date_closed}'
+
+    class Meta:
+        db_table = 'project'
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
 
 
 
